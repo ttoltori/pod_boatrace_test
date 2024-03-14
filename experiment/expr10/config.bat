b72a9c4059bd0602762b56db0f9fe91c3e361ec0
ttolt <ttolt@DESKTOP-3TF8182>
2024-03-14 23:45:46 +0900

    중간커밋

--- experiment/expr10/config.bat
+++ experiment/expr10/config.bat
@@ -1,4 +1,4 @@
 @echo off
 @rem 環境変数設定
-set CP=C:/Dev/github/pod_boatrace/target/classes;C:/Dev/workspace/Oxygen/pod_boatrace_test/lib/*;.
+set CP=C:/Dev/github/pod_boatrace/target/classes;C:/Dev/github/pod_boatrace_test/lib/*;.
 set PROPERTIES=C:/Dev/github/pod_boatrace/properties/expr10/expr10.properties
--- experiment/expr10/ml_server_py.bat
+++ experiment/expr10/ml_server_py.bat
@@ -7,7 +7,7 @@ set starttime=%date%_%time%
 set PYTHONPATH=%PYTHONPATH%;C:/Dev/github/py_boatrace
 set program_property=C:/Dev/github/pod_boatrace/properties/expr10/expr10.properties
 set model_property=C:/Dev/github/pod_boatrace/properties/expr10/model.properties
-
+`
 python C:/Dev/github/py_boatrace/boatrace/server/BoatWebSocketServer.py %program_property% %model_property% 
 
 set endtime=%date%_%time%
